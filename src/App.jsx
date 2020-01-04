9faa8f02e87f48ca399b68e305b45025d1acaaec
pragyakar <pragyakar75@gmail.com>
2020-01-04 16:06:41 +0545

    :sparkles: Added Auth component and auth logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import Auth from './Components/Auth';
 
-const App = () => (    
-  <div className="App">
-    Outlay      
-  </div>
-);
+const App = () => {
+
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    setAuthenticated(true);
+  }, []);
+
+  return (    
+    <div className="App">
+      { authenticated ? <p>Dashboard</p> : <Auth /> }
+    </div>
+  )
+};
 
 export default App;
---
+++ src/Components/Auth/Auth.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Auth = () => {
+  return (
+    <p>Login</p>
+  );
+}
+
+export default Auth;
