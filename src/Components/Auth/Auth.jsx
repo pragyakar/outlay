9bddd21ff694ad449441e0ee38c870101f096b79
pragyakar <pragyakar75@gmail.com>
2020-01-04 22:20:49 +0545

    :hammer: Changed auth flow

--- src/Components/Auth/Auth.jsx
+++ src/Components/Auth/Auth.jsx
@@ -7,7 +7,7 @@ const Auth = (props) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (username.toLowerCase() === "pragyakar") {
+    if (username.toLowerCase() === "pragyakar" && password.toLowerCase() === "pragyakar") {
       props.setUser(username);
       props.setAuthenticated(true);
     } else {
