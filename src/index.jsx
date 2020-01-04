80beae2593c1bb3b83418d6e458c710cdb0fcad7
pragyakar <pragyakar75@gmail.com>
2020-01-04 15:14:38 +0545

    :wrench: Moved App.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css';
 
-const App = () => {
-  return (
-    <div>
-      Outlay    
-    </div>
-  );
-};
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
