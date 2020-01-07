b4c9cb1acc5b05edb0d255fa36b1ab912180a76e
pragyakar <pragyakar75@gmail.com>
2020-01-07 16:56:44 +0545

    :hammer: Moved firebase configuration to environment variables

--- src/env.js
+++ src/env.js
@@ -1,5 +1,12 @@
 const Env = {
- testToken: envConfig.TEST_TOKEN
+  testToken: envConfig.TEST_TOKEN,
+  firebaseApiKey: envConfig.FIREBASE_API_KEY,
+  firebaseAuthDomain: envConfig.FIREBASE_AUTH_DOMAIN,
+  firebaseDatabaseUrl: envConfig.FIREBASE_DATABASE_URL,
+  firebaseProjectId: envConfig.FIREBASE_PROJECT_ID,
+  firebaseStorageBucket: envConfig.FIREBASE_STORAGE_BUCKET,
+  firebaseMessageSenderId: envConfig.FIREBASE_MESSAGE_SENDER_ID,
+  firebaseAppId: envConfig.FIREBASE_APP_ID
 }
 
 export default Env;
