2d21976f7313546244338e596879518681d1ed44
pragyakar <pragyakar75@gmail.com>
2020-01-07 23:00:48 +0545

    :package: Added moment and date parsing

--- src/Components/Dashboard/ContentPanel/Records/History/Table/TableRow.jsx
+++ src/Components/Dashboard/ContentPanel/Records/History/Table/TableRow.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import moment from 'moment';
 
 const TableRow = (props) => {
   return (
     <div className="history-table-row">
-      <div>{props.timestamp}</div>
+      <div>{moment(+props.timestamp).format("MMM Do YYYY")}</div>
       <div>NRs. {props.amount}</div>
       <div>{props.remarks}</div>
       <div>Edit | Delete</div>
