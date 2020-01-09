2fe3201e0a6328c52aff77c70cad23cf06db34cc
pragyakar <pragyakar75@gmail.com>
2020-01-09 12:12:44 +0545

    :sparkles: Display dynamic username in Welcome component

--- src/Components/Dashboard/Welcome/Welcome.jsx
+++ src/Components/Dashboard/Welcome/Welcome.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
-const Welcome = () => {
+const Welcome = (props) => {
+
+  const { user } = props; 
   return (
     <div className="welcome-container">
-      <p className="welcome-title">👋 Welcome back Pragyakar</p>
+      <p className="welcome-title">👋 Welcome back {capitalize(user)}</p>
       <p className="welcome-subtitle">Look at all the money you are spending and regret. Such a shame.</p>
     </div>
   )
 }
+
+const capitalize = (word) => {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+};
 
 export default Welcome;
