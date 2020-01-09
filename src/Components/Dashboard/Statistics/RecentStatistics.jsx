439e02c07d9f5ffd67bfa26bd314ff35f4c07623
pragyakar <pragyakar75@gmail.com>
2020-01-09 12:10:07 +0545

    :hammer: Implemented hooks in RecentStats component

--- src/Components/Dashboard/Statistics/RecentStatistics.jsx
+++ src/Components/Dashboard/Statistics/RecentStatistics.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import StatBox from './StatBox';
 import moment from 'moment';
 
@@ -8,45 +8,54 @@ const RecentStatistics = (props) => {
   const today = moment().startOf('day').format("x");
   const oneWeekAgo = moment().subtract(7, 'days').format("x");
   const oneMonthAgo = moment().subtract(30, 'days').format("x");
+  const [todaysExpenses, setTodaysExpenses] = useState(0);
+  const [weeksExpenses, setWeeksExpenses] = useState(0);
+  const [monthsExpenses, setMonthsExpenses] = useState(0);
   
-  const todaysExpenses = () => {
-    let total = 0;
-    expenses && expenses.map((expense) => {
-      if (expense.timestamp > today) {
-        total += +expense.amount
-      }
-    });
-    return total;
-  }
+  useEffect(() => {
+    setTodaysExpenses(getTodaysExpenses(expenses, today));
+    setWeeksExpenses(getWeeksExpenses(expenses, oneWeekAgo));
+    setMonthsExpenses(getMonthsExpenses(expenses, oneMonthAgo));
+  }, []); 
 
-  const weeksExpenses = () => {
-    let total = 0;
-    expenses && expenses.map((expense) => {
-      if (expense.timestamp > oneWeekAgo) {
-        total += +expense.amount
-      }
-    });
-    return total;
-  }
-
-  const monthsExpenses = () => {
-    let total = 0;
-    expenses && expenses.map((expense) => {
-      if (expense.timestamp > oneMonthAgo) {
-        total += +expense.amount
-      }
-    });
-    return total;
-  }
-  
   return (
     <div className="statistics-container">
       <h1 className="statistics-title">Recent Stats 🕒</h1>
-      <StatBox label={"Today"} amount={todaysExpenses()} />
-      <StatBox label={"Last 7 days"} amount={weeksExpenses()} />
-      <StatBox label={"Last 30 days"} amount={monthsExpenses()} />
+      <StatBox label={"Today"} amount={todaysExpenses} />
+      <StatBox label={"Last 7 days"} amount={weeksExpenses} />
+      <StatBox label={"Last 30 days"} amount={monthsExpenses} />
     </div>
   )
 };
+
+const getTodaysExpenses = (expenses, today) => {
+  let total = 0;
+  expenses && expenses.map((expense) => {
+    if (expense.timestamp > today) {
+      total += +expense.amount
+    }
+  });
+  return total;
+}
+
+const getWeeksExpenses = (expenses, oneWeekAgo) => {
+  let total = 0;
+  expenses && expenses.map((expense) => {
+    if (expense.timestamp > oneWeekAgo) {
+      total += +expense.amount
+    }
+  });
+  return total;
+}
+
+const getMonthsExpenses = (expenses, oneMonthAgo) => {
+  let total = 0;
+  expenses && expenses.map((expense) => {
+    if (expense.timestamp > oneMonthAgo) {
+      total += +expense.amount
+    }
+  });
+  return total;
+}
 
 export default RecentStatistics;
