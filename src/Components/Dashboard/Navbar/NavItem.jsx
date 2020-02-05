bcf7b512931c4cfea0a49318cbc45e74951ee14f
pragyakar <pragyakar75@gmail.com>
2020-02-05 17:54:56 +0545

    :sparkles: Indicate active tab in navigation

--- src/Components/Dashboard/Navbar/NavItem.jsx
+++ src/Components/Dashboard/Navbar/NavItem.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useLocation } from "react-router-dom";
 
 const NavItem = (props) => {
+
   const { icon, path } = props;
+
+  const { pathname } = useLocation();
+  const currentPath = pathname.split('/')[1];
+  
+  const isActiveTab = (path, currentPath) => {
+    if (path === currentPath) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   return (
     <Link to={`/${path}`}>
-      <div className="navItem">
+      <div className={`navItem ${isActiveTab(path, currentPath) ? "active" : ""}`}>
         <FontAwesomeIcon icon={icon} />  
       </div>
     </Link>
