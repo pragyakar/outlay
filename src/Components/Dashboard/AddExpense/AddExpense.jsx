45bd5cbd820526bc9ef3a5a08e5d2b746233b62b
pragyakar <pragyakar75@gmail.com>
2020-02-06 10:30:10 +0545

    :sparkles: Added add expense button component

---
+++ src/Components/Dashboard/AddExpense/AddExpense.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const AddExpense = () => {
+  return (
+    <div className="add-button">
+      <FontAwesomeIcon icon={'plus'} />  
+    </div>
+  );
+}
+
+export default AddExpense;
--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import '../../index.css';
 import Navbar from './Navbar';
 import DashboardRouter from './DashboardRouter';
+import AddExpense from './AddExpense';
 
 const Dashboard = () => {
   return (
     <div className="root-container">
       <BrowserRouter>
         <Navbar />
+        <AddExpense />
         <div className="body-container">
           <DashboardRouter />
         </div>
